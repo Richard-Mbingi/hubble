d7d34d1005f1a436b389e0b8c4fff7e321e29a5e
Richard-Mbingi <mbingicoding@gmail.com>
2021-09-24 11:26:04 +0300

    Theme configuration and global styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
+import { ThemeProvider } from "styled-components";
+import Header from "./components/Header";
+import { Container } from "./components/styles/Container.styled";
+import GlobalStyles from "./components/styles/Global";
+
+const theme = {
+  colors: {
+    header: '#ebfbff',
+    body: '#fff',
+    footer: '#003333'
+  }
+}
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
+    <ThemeProvider theme = {theme}>
+      <>
+      <GlobalStyles />
+        <Header></Header>
+        <Container>
+          <h1>Hello world</h1>
+        </Container>
+      </>
+    </ThemeProvider>
   );
 }
 
